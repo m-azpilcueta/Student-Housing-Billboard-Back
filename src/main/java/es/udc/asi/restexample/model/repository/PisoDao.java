02aa6ecea8656a0b768968da9f26b80d0bceeaaa
martinazpilcueta <m.azpilcueta@udc.es>
2021-12-01 18:02:16 +0100

    Interfaz DAO de Piso. TO-DO: Método(s) para filtrados

---
+++ src/main/java/es/udc/asi/restexample/model/repository/PisoDao.java
@@ -0,0 +1,17 @@
+package es.udc.asi.restexample.model.repository;
+
+import es.udc.asi.restexample.model.domain.Piso;
+
+import java.util.List;
+
+public interface PisoDao {
+  List<Piso> findAll();
+
+  Piso findById(Long id);
+
+  void create(Piso piso);
+
+  void update(Piso piso);
+
+  void deleteById(Long id);
+}
